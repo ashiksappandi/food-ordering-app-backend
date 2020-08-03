ff6d478bfae3ac9ae94229a06c409fc714d59d2b
anup-shanbhag <anupshanbhag.sd11@iiitb.net>
2020-08-03 19:41:33 +0200

    Added Implementation for ItemEntity

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
@@ -1,5 +1,129 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
-public class ItemEntity {
-    // TODO
+import org.apache.commons.lang3.builder.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "item")
+public class ItemEntity implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "itemIdGenerator")
+    @SequenceGenerator(name = "itemIdGenerator", sequenceName = "item_id_seq")
+    @ToStringExclude
+    @HashCodeExclude
+    private Integer id;
+
+    @Column(name = "uuid")
+    @NotNull
+    @Size(max = 200)
+    private Integer uuid;
+
+    @Column(name = "item_name")
+    @NotNull
+    @Size(max = 30)
+    private String itemName;
+
+    @Column(name = "price")
+    @NotNull
+    private Integer price;
+
+    @Column(name = "type")
+    @Size(max = 10)
+    @NotNull
+    private String type;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "category_item",
+            joinColumns = {@JoinColumn(name = "item_id")},
+            inverseJoinColumns = {@JoinColumn(name = "category_id")})
+    private Set<CategoryEntity> categories = new HashSet<CategoryEntity>();
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    @NotNull
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ToStringExclude
+    @HashCodeExclude
+    @EqualsExclude
+    Set<OrderItemEntity> orders = new HashSet<OrderItemEntity>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Integer uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Set<CategoryEntity> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<CategoryEntity> categories) {
+        this.categories = categories;
+    }
+
+    public Set<OrderItemEntity> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(Set<OrderItemEntity> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this,obj,Boolean.FALSE);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this,Boolean.FALSE);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
 }
