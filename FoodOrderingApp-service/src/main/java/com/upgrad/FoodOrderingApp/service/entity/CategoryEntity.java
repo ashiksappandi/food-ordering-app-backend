689c4c0a173448cb813a6662efaa589a3398c17b
anup-shanbhag <anupshanbhag.sd11@iiitb.net>
2020-08-03 19:38:49 +0200

    Added Implementation for CategoryEntity

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryEntity.java
@@ -1,5 +1,69 @@
 package com.upgrad.FoodOrderingApp.service.entity;
 
-public class CategoryEntity {
-    // TODO
+import org.apache.commons.lang3.builder.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoryIdGenerator")
+    @SequenceGenerator(name = "categoryIdGenerator", sequenceName = "category_id_seq")
+    @ToStringExclude
+    @HashCodeExclude
+    private Integer id;
+
+    @Column(name = "uuid")
+    @NotNull
+    @Size(max = 200)
+    private Integer uuid;
+
+    @Column(name = "category_name")
+    @Size(max = 30)
+    private String categoryName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(Integer uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this,obj,Boolean.FALSE);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this,Boolean.FALSE);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+
 }
